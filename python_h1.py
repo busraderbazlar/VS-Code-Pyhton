2171e96f284d892b1afd47aed33844fcbe84213b
busraderbazlar <busra.derbazlar@gmail.com>
2021-12-31 16:17:38 +0300

    06112021

--- python_h1.py
+++ python_h1.py
@@ -33,85 +33,87 @@
 # 18. Sipariş Id'ye göre arama yapılacak
 # 19. Menü hazırlıkları yapılacak
 ###############################################################
-
-"""
 # 1 → Kütüphaneleri import edelim
 # API request", 
 # "URL parse", 
 # "epoch time conversion"
-import #<Gerekli Kütüphaneler İle Değiştir>
-
+import requests
+from urllib.parse import urlparse
+import datetime
+import time
+import json
+from datetime import datetime, timedelta
+import googlemaps
+#print(time.ctime(857344273))
 
 # 2 → Customers API için gerekli URL girelim
-urlCustomers = "<Gerekli URL İle Değiştir>"
-
 # 3 → Customers API için GET request
-rCustomers = #<Request>
-
 # 4 → if  case, bu kısımda her hangi bir kod değişikliği yapmayın lütfen
+# 5 → Response içeriği print et referans amaçlı
+urlCustomers = "https://northwind.netcore.io/customers.json"
+rCustomers = requests.get(urlCustomers)
 if not rCustomers.status_code == 200:
     raise Exception("API Bağlantı Sorunu. Status code: {}. Text: {}".format(
         rCustomers.status_code, rCustomers.text))
-
-# 5 → Response içeriği print et referans amaçlı
 #print(rCustomers.text)
 
 # 6 → Response icerigi json data formatına encode et
-jsonCustomers = #<Gerekli Json Encode Kodu İle Değiştir>
+jsonCustomers = rCustomers.json()
+#print(jsonCustomers)
+#print(jsonCustomers.values)
 
 # 7 → Orders API için gerekli URL'leri girelim
-urlOrders = "<Gerekli URL İle Değiştir>"
-
 # 8 → Orders API için GET request
-rOrders = #<Request>
-
 # 9 → if  case, bu kısımda her hangi bir kod değişikliği yapmayın lütfen
+# 10 → Response içeriği print et referans amaçlı
+urlOrders = "https://northwind.netcore.io/orders.json"
+rOrders = requests.get(urlOrders)
 if not rOrders.status_code == 200:
     raise Exception("API Bağlantı Sorunu. Status code: {}. Text: {}".format(
         rOrders.status_code, rOrders.text))
-
-# 10 → Response içeriği print et referans amaçlı
 #print(rOrders.text)
 
 # 11 → response icerigi json data formatına encode et
-jsonOrders = #<Gerekli Json Encode Kodu İle Değiştir>
+jsonOrders = rOrders.json()
+#print(jsonOrders)
 
 # 12 → Mapquest API için gerekli URL'leri girelim
-mainMapApiUrl="<Gerekli URL İle Değiştir>"
-
 # 13 → Mapquest Credential için; token, key... hazırlıkları yapalım
-mapApiKey="Kisisel_Key"
-
+mainMapApiUrl="http://www.mapquestapi.com/directions/v2/route?key=yAApx2G1tnQAa3QSNsIU0CdvnB3ZfVoA&from=Paris&to=Marseille"
+mapApiKey="yAApx2G1tnQAa3QSNsIU0CdvnB3ZfVoA"
 
 # 14 → UI manipulasyonu. Output anında 15 karakterden uzun metinler için manipulasyon yapalım
 def metinKontrol(metin):
     if len(metin)>15:
-        return f"..."
+        degisim = metin.replace(metin[16:],"...") 
+        return f"{degisim}"
     else:
-        return f"({metin})"
+        return f"{metin}"
 
 # 15 → Müşterileri listeleyelim
+
 def musteriListele():
     print("Müşteri Listesi")
     print("+--------+-----------------------+-----------------------+-----------------------+-----------------------+-----------------------+")
     print("|ID      |CompanyName            |ContactName            |Address                |Country                |City                   |")
     print("+--------+-----------------------+-----------------------+-----------------------+-----------------------+-----------------------+")
-
-    for i in jsonCustomers["<Gerekli Key İle Değiştir>"]:
-        print(f"Gerekli Output İçin Formatla")
-
-    print("+--------+-----------------------+-----------------------+-----------------------+-----------------------+-----------------------+")
+"""        
+for i in jsonCustomers["results"]:
+    print(f"{i['id']}\t{i['companyName']}\t{i['contactName']}\t{i['address']}\t{i['country']}\t{i['city']}\t", end=" ")
+    print("+--------+-----------------------+-----------------------+-----------------------+-----------------------+-----------------------+")"""
 
 # 16 → Müşterileri Id'ye göre arama yapalım
 def musteriAra(musteriId):
-    for i in jsonCustomers["<Gerekli Key İle Değiştir>"]:
+    for i in jsonCustomers["results"]:
         if i['id']==musteriId:
             print(f"{musteriId} ID'li müşteri bulundu. Detay Listesi")
             print("==========================")
-            print(f"Gerekli Outputlar İçin Değiştir")          
+            metinKontrol(i)
+            print(f"Şirketinin adı: {i['companyName']}\nUlaşılacak kişi adı: {i['contactName']}\nAdresi: {i['address']}\nÜlkesi: {i['country']}\nŞehri: {i['city']}")          
             break
-    else:
-        print(f"{musteriId} ID'li müşteri bulunamadı")
+        else:
+            print(f"{musteriId} ID'li müşteri bulunamadı")
+
 
 # 17 → Siparişleri listeleyelim
 def siparisListele():  
@@ -119,33 +121,45 @@ def siparisListele():
     print("+--------+---------------+-------------------------------+-----------------------+-----------------------+-----------------------+")
     print("|ID      |CustomerId     |OrderDate                      |ShipAddress            |ShipCity               |ShipCountry            |")
     print("+--------+---------------+-------------------------------+-----------------------+-----------------------+-----------------------+")
-    for i in jsonOrders["<Gerekli Key İle Değiştir>"]:
-        epochSaniye = #<Gerekli Key İle Değiştir>
-        gunumuzZamani = #<Time Kütüphanesi Gerekli Fonksiyon İle Değiştir>
-        print(f"")
-    print("+--------+-----------------------+-----------------------+-----------------------+-----------------------+-----------------------+")
+"""for i in jsonOrders["results"]:
+    risetimeInEpochSeconds = int(i['order']["orderDate"][6:15])
+    risetimeInFormattedString = time.ctime(risetimeInEpochSeconds)
+print(f"{i['order']['id']}\t{i['order']['customerId']}\t{risetimeInFormattedString}\t{i['order']['shipAddress']}\t{i['order']['shipCity']}\t{i['order']['shipCountry']}\t", end=" ")
+print("+--------+-----------------------+-----------------------+-----------------------+-----------------------+-----------------------+")"""
 
 # 18 → Sipariş Id'ye göre arama yapalım
 def siparisAra(siparisId):
-    for i in jsonOrders["<Gerekli Key İle Değiştir>"]:
+    for i in jsonOrders["results"]:
         if i['order']['id']==siparisId:
-            epochSaniye = #<Gerekli Key İle Değiştir>
-            gunumuzZamani = #<Time Kütüphanesi Gerekli Fonksiyon İle Değiştir>
+            risetimeInEpochSeconds = int(i['order']["orderDate"][6:15])
+            risetimeInFormattedString = time.ctime(risetimeInEpochSeconds)
             print(f"{siparisId} ID'li sipariş bulundu. Detay Listesi")
             print("==========================")
-            print(f"Gerekli Outputlar İçin Değiştir") 
-            nereye = #<Gerekli Key İle Değiştir>          
+            print(f"Sipariş id : {i['order']['id']}\nMüşteri id : {i['order']['customerId']}\nSipariş tarihi : {risetimeInFormattedString}\nSipariş adresi : {i['order']['shipAddress']}\nŞehri : {i['order']['shipCity']}\nÜlkesi : {i['order']['shipCountry']}\n")
+            nereye = i['order']['shipCity']          
             cevap = input(f"Kargo Rotasını {nereye.upper()} Şehri İçin Görmek İster misiniz? [e/E] :")
             if cevap.lower()=="e":
                 while True:                    
                     print(f"Varış Noktası {nereye} için Rota Hesaplanacak")
                     nereden = input("Nereden Çıkacak: ")
-                    #<MapQuest API'yı Kullanarak Rota Belirlenecek>
+                    mainMapApiUrl="http://www.mapquestapi.com/directions/v2/route?key=yAApx2G1tnQAa3QSNsIU0CdvnB3ZfVoA&from="+nereden+"&to="+nereye+"\""""
+                    mapApi=requests.get(mainMapApiUrl)
+                    mapApiorders= mapApi.json()
+                    print("==========================")
+                    print(f"Toplam km: {mapApiorders['route']['distance']}")
+                    print(f"Toplam süre : {mapApiorders['route']['time']}")
+                    print("==========================")
+                    for i in mapApiorders['route']['legs']:
+                        print(i['destNarrative'])
+                        print(i['origNarrative'])
+                        for a in i['maneuvers']:
+                            print(f"{a['narrative']}")
                     break
             break
     else:
         print(f"{siparisId} ID'li sipariş bulunamadı")
-# endregion
+
+
 
 # 19 → menu
 while True:
@@ -160,14 +174,23 @@ while True:
     [5]     → ÇIK
     """))
     if secim==1:
-        pass
+        musteriListele()
+        for i in jsonCustomers["results"]:
+            metinKontrol(i['address'])
+            print(f"|{i['id']}|\t|{i['companyName']}|\t|{i['contactName']}|\t|{i['address']}|\t|{i['country']}|\t|{i['city']}|\n",end="")
+        print("+--------+-----------------------+-----------------------+-----------------------+-----------------------+-----------------------+")
     elif secim==2:
-        pass
+        musteriAra(input("Müşteri id giriniz:"))
     elif secim==3:
-        pass
+        siparisListele()
+        for i in jsonOrders["results"]:
+            risetimeInEpochSeconds = int(i['order']["orderDate"][6:15])
+            risetimeInFormattedString = time.ctime(risetimeInEpochSeconds)
+            print(f"|{i['order']['id']}|\t|{i['order']['customerId']}|\t|{risetimeInFormattedString}|\t|{i['order']['shipAddress']}|\t|{i['order']['shipCity']}|\t|{i['order']['shipCountry']}|\n",end=" ")
+        print("+--------+-----------------------+-----------------------+-----------------------+-----------------------+-----------------------+")
     elif secim==4:
-        pass
+        siparisAra(int(input("Lütfen Sipariş id giriniz:")))
     elif secim==5:
-        pass
+        break
     else:
-        pass"""
+        pass
